2a9248ecd3c90a93a4180ad4c0e37e4093cfeaa1
vishalgajage09 <gajagevishal09@gmail.com>
2021-04-12 06:53:38 +0530

    First Intro

---
+++ simple.py
@@ -0,0 +1,23 @@
+numbers=[12,24,36,48,60,72,84,96]
+
+#numbers.insert(3,30)
+#numbers.remove(60)
+#numbers.sort()
+#numbers.reverse()
+#print(numbers[1:4])
+#print(numbers[1:5:2])
+#numbers.pop()
+#numbers.append(22)
+#numbers.append(50)
+#print(len(numbers))
+#print(max(numbers))
+#print(min(numbers))
+#print(numbers)
+#tuple=(1,2,3)
+#tuple[2]=20
+#print(tuple)
+a = 2
+b = 5
+print(a,b)
+a,b=b,a
+print(a,b)
